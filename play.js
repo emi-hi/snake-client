72a99f80f8d9f25ec365eb5b2468f925ee93507e
emi-hi <emily.hillier@gmail.com>
2019-09-25 13:51:45 -0700

    added ctrl c exit

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
+
+const handleUserInput = function(key){
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.once('data', handleUserInput);
+  stdin.resume();
+  return stdin;
+}
+
 connect();
+setupInput();
