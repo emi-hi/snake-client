f6bda754eb4d18f4857e04f403c896af19685e0a
emi-hi <emily.hillier@gmail.com>
2019-09-25 13:55:56 -0700

    split the input functions out to a new file and exported the userInput function. Added require to play

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 
-const handleUserInput = function(key){
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.once('data', handleUserInput);
-  stdin.resume();
-  return stdin;
-}
 
 connect();
 setupInput();
